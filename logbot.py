1fe08609e82eee5bc064ff26d4a677c90816b96b
Kundhan <kundhanakkenapally@gmail.com>
2026-03-14 16:28:40 +0530

    Refactor token handling to use environment variable
    Updated token retrieval to use environment variable.

--- logbot.py
+++ logbot.py
@@ -3,7 +3,9 @@ from discord.ext import commands
 from datetime import datetime
 import asyncio
 
-TOKEN = "MTQ4MjIxNzkxMzM2MzczMDQ4NA.Gzma9t.u3PZu70zDexcbgwU8UTPesKnQY8AE3lAAVmeD8"  # Replace with your actual token
+import os
+TOKEN = os.getenv("TOKEN")
+
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
